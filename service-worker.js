15d5927fee1eb72003847d40c519367bde61fcb2
TDiesel <45299267+TDiesel@users.noreply.github.com>
2018-11-29 18:48:23 -0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const HOST='https://tdiesel.github.io/noticia/'
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register(`${HOST}/service-worker.js`)
+    .then(function () { console.log('Service Worker Registered'); });
+}
+
+var cacheName = 'agenda-app';
+var filesToCache = [
+  `${HOST}/imagens/edit.png`,
+  `${HOST}/imagens/remove.png`,
+  `${HOST}/imagens/icons/icon-128x128.png`,
+  `${HOST}/imagens/icons/icon-144x144.png`,
+  `${HOST}/imagens/icons/icon-152x152.png`,
+  `${HOST}/imagens/icons/icon-192x192.png`,
+  `${HOST}/imagens/icons/icon-256x256.png`,
+  `${HOST}/lib/bulma.css`,
+  `${HOST}/app.css`,
+  `${HOST}/index.html`,
+  `${HOST}/store.js`,
+  `${HOST}/todo_e6.js`,
+  `${HOST}/service-worker.js`,
+  `${HOST}/manifest.json`
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(cacheName).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
